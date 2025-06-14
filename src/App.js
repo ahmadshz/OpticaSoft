66bc7ea1e0090a17d4f4bc693aced289735a6f54
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-06-14 12:22:32 +0300

    update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Contact from './Components/Contact/Contact';
 import { TbArrowBigUpLines } from 'react-icons/tb';
+import { MdKeyboardDoubleArrowUp } from 'react-icons/md';
+import { RiArrowUpSLine } from 'react-icons/ri';
 
 function App() {
   const [showArrow, setShowArrow] = useState(false);
@@ -25,7 +27,7 @@ function App() {
         setShowArrow(false)
       }
     }
-     window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScroll)
     return () => {
       window.removeEventListener('scroll', handleScroll)
     }
@@ -42,10 +44,14 @@ function App() {
       </Routes>
       <Footer />
       <div
-        className={`fixed bottom-4 right-4 bg-[#102D44] p-2 md:p-4 rounded-full text-white transition-opacity duration-300 ${showArrow ? 'opacity-100' : 'opacity-0'
-          }`}
+       className={`fixed bottom-6 right-4 md:bottom-8 md:right-8 bg-white p-2 rounded rotate-45  shadow-2xl shadow-black transition-opacity duration-300
+                   ${showArrow ? 'opacity-100' : 'opacity-0'}`}
+
+                    style={{
+                      boxShadow: '0 0 20px rgba(0, 0, 0, 0.3)',
+                    }}
       >
-        <TbArrowBigUpLines size={35} onClick={handlegoToTop} />
+        <RiArrowUpSLine className='-rotate-45 text-[20px] '   onClick={handlegoToTop} />
       </div>
     </div>
   );
