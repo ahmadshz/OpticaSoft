92a744c67c928b6563376a62444ab8e41155955a
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-06-29 14:15:33 +0300

    add project

--- src/1-Utils/data.js
+++ src/1-Utils/data.js
@@ -1,5 +1,6 @@
 import dallal from '../assests/projects/dallal.png'
 import bite from '../assests/projects/gegabite.png'
+import villahouse from '../assests/projects/villahouse.png'
 
 
 
@@ -7,7 +8,7 @@ import bite from '../assests/projects/gegabite.png'
 
 export const MyProject = [
   {
-    id: 1,
+    id: 0,
     name: 'Dallal Sy',
     desc: " Dallal Sy is an online platform proudly developed by our team of dedicated developers and designers. We built Dallal to be a smart, dynamic space where users can easily post and browse everything from cars and real estate to jobs, services, pets, and more. We focused on performance, accessibility, and smooth user experience to make the platform fast, reliable, and easy to use.",
     imgPath: dallal,
@@ -19,6 +20,13 @@ export const MyProject = [
     desc: " We created a fast, elegant restaurant website that helps customers find and order their favorite meals in seconds. From browsing delicious food categories to viewing detailed menus and placing orders with location tracking — everything is smooth, mobile-friendly, and built to drive real customer engagement. Your restaurant, one click away.",
     imgPath: bite,
     link: 'https://gega-bite.vercel.app',
+  },
+  {
+    id: 2,
+    name: 'Villahouse',
+    desc: "A professional real estate website I developed for VillaHouse in the UAE, specializing in showcasing luxury villas and homes. I focused on user experience, modern design, and full responsiveness across all devices.",
+    imgPath: villahouse,
+    link: 'https://villahouse.ae/',
   },
  
  
